df85ee308cc5d2d950606e574971f94523358318
Ogeu <thibautguesdon@gmail.com>
2021-03-03 09:04:47 +0100

    recherche-multimedia orga

--- recherche-multimedia.php
+++ recherche-multimedia.php
@@ -4,20 +4,20 @@
 
    if (isset($_GET["search"]))
    {
-    $_GET["search"] = htmlspecialchars($_GET["search"]);    //pour sécuriser le formulaire contre les intrusions html
-                        $search = $_GET["search"];
-                        $search = trim($search);                                  //pour supprimer les espaces dans la requête de l'internaute
-                        $search = strip_tags($search);                            //pour supprimer les balises html dans la requête
+    $_GET["search"] = htmlspecialchars($_GET["search"]); //pour sécuriser le formulaire contre les intrusions html
+    $search = $_GET["search"];
+    $search = trim($search);                             //pour supprimer les espaces dans la requête de l'internaute
+    $search = strip_tags($search);                       //pour supprimer les balises html dans la requête
 
-                    if (isset($search))
-                        {
-                        $req = $bdd->getStart()->prepare("SELECT * FROM media WHERE Titre LIKE ?");
-                        $req->execute(array("%".$search."%"));
-                        }
-                    else
-                        {
-                        $message = "Vous devez entrer votre requete dans la barre de recherche";
-                        }
+   if (isset($search))
+     {
+        $req = $bdd->getStart()->prepare("SELECT * FROM media WHERE Titre LIKE ?");
+        $req->execute(array("%".$search."%"));
+     }
+   else
+     {
+        $message = "Vous devez entrer votre requete dans la barre de recherche";
+     }
     }
 
 ?>
