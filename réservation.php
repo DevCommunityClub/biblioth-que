c84c4319604746675a57b17f1e1fcebd793459d5
Retr0 <48931555+Retr0Lovelace@users.noreply.github.com>
2021-03-03 16:05:48 +0100

    Update réservation.php

--- réservation.php
+++ réservation.php
@@ -27,40 +27,33 @@
           <input type = "submit" name = "search" value = "Rechercher">
         </form>
       </div>
+        <?php
+        $function = new Functions();
+        $function->fetch_media();
+        $a = $function->getReq();
 
+        $count_1 = count($a);
+        for ($i = 0; $i < $count_1; $i++) {
+        ?>
         <div class="card mb-3" id = "Item_res">
-          <div class="row g-0">
-            <div class="col-md-4">
-              <?php
-                $function = new Functions();
-                $function->fetch_media();
-                $a = $function->getReq();
-                var_dump($a);
-                die();
-              ?>
-
-                <div class="card mb-3" id = "Item_res">
-                  <div class="row g-0">
-                    <div class="col-md-4">
-                    <?php
-                      $count_1 = count($a);
-                      for ($i=0; $i <$count_1 ; $i++) {
-                        echo '<img src='.$a[$i]['Lien_image'].'alt="..." style ="width : 200px" id ="img-size">';
-                        echo'</div>';
-                        echo'<div class="col-md-8">';
-                        echo'<div class="card-body">';
-                        echo'<h1 class="card-title">'.$a[$i]['Titre'].'</h1>';
-                        echo'<p class="card-text">'.$a[$i]['Description'].'</p>';
-                        echo'<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>';
-                        echo'<a href=" class="btn btn-primary btn-user btn-block"> Réservez maintenant';
-                        echo'</div>';
-                      }
-                    ?>
-                  </div>
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="<?php echo $a[$i]['Lien_image']; ?>" alt="..." style ="width: 200px" id ="img-size">
                 </div>
-            </section><!-- End Breadcrumbs -->
-
-
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h1 class="card-title"><?php echo $a[$i]['Titre']; ?></h1>
+                        <p class="card-text"><?php echo $a[$i]['Description']; ?></p>
+                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        <a href="" class="btn btn-primary btn-user btn-block">
+                            Réservez maintenant
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php } ?>
+    </section><!-- End Breadcrumbs -->
 
     <!-- ======= Pricing Section ======= -->
     <section id="pricing" class="pricing">
