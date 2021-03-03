2b1a7984a5af068fd934f70fd601e82ea40402c7
Retr0 <48931555+Retr0Lovelace@users.noreply.github.com>
2021-03-03 08:50:30 +0100

    recherche-multimedia

--- recherche-multimedia.php
+++ recherche-multimedia.php
@@ -1,26 +1,18 @@
 <?php
-    try
-        {
-        $bdd = new PDO("mysql:host=localhost;dbname=library", "root", "");
-        $bdd ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        }
-        catch(Exception $e)
-            {
-            die("Une érreur a été trouvé : " . $e->getMessage());
-                }
-                    $bdd->query("SET NAMES UTF8");
+    require_once 'bdd.php';
+    $bdd = new bdd();
 
-                    if (isset($_GET["search"]))
-                    {
-                        $_GET["search"] = htmlspecialchars($_GET["search"]);    //pour sécuriser le formulaire contre les intrusions html
+   if (isset($_GET["search"]))
+   {
+    $_GET["search"] = htmlspecialchars($_GET["search"]);    //pour sécuriser le formulaire contre les intrusions html
                         $search = $_GET["search"];
                         $search = trim($search);                                  //pour supprimer les espaces dans la requête de l'internaute
                         $search = strip_tags($search);                            //pour supprimer les balises html dans la requête
 
                     if (isset($search))
                         {
-                        $req = $bdd->prepare("SELECT * FROM media WHERE Titre LIKE ?");
-                        $req->execute(array("%".$search."%", "%".$search."%"));
+                        $req = $bdd->getStart()->prepare("SELECT * FROM media WHERE Titre LIKE ?");
+                        $req->execute(array("%".$search."%"));
                         }
                     else
                         {
